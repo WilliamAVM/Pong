b203aa1b801ef218ae6b5e0a4da4fc5b4e16e1a9
williamVMA <William18_z95@hotmail.com>
2017-06-03 02:39:48 -0400

    asdasd

---
+++ Ping-Pong/src/Vista/Menu.java
@@ -0,0 +1,57 @@
+package Vista;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+public class Menu extends JFrame{
+	
+	private JPanel panel;
+	private Color colorRojo;
+	private JLabel fondoParaGif, titulo;
+	private JButton jugar, ajustes, salir; 
+	public Menu(){
+		setResizable(false);
+		setTitle("Ping Pong");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(500, 500, 900, 600);
+		setLocationRelativeTo(null);
+		
+		colorRojo= new Color(213, 44, 60);
+		panel = new JPanel(null);
+		panel.setBackground(Color.BLACK);
+		setContentPane(panel);
+		
+		titulo= new JLabel();
+		titulo.setIcon(new javax.swing.ImageIcon("C:/Users/willi/workspace/Ping Pong/Imagenes/Titulo.png"));
+		titulo.setBounds(290, -200, 400, 500);
+		panel.add(titulo);
+		
+		salir= new JButton("Salir");
+		salir.setBackground(colorRojo);
+		salir.setForeground(Color.WHITE);
+		salir.setFont(new Font("Roboto", Font.BOLD, 30));
+		salir.setBounds(320, 500, 105, 30);
+		panel.add(salir);
+		
+		fondoParaGif = new JLabel();
+		fondoParaGif.setIcon(new javax.swing.ImageIcon("C:/Users/willi/workspace/Ping Pong/Imagenes/fondoPrincipal.gif"));
+		fondoParaGif.setBounds(0,0, 900, 600);
+		panel.add(fondoParaGif);
+		
+		
+		
+		
+	}
+}
---
+++ Ping-Pong/src/Vista/main.java
@@ -0,0 +1,9 @@
+package Vista;
+
+public class main {
+
+	public static void main(String arg[]){
+		Menu m = new Menu();
+		m.setVisible(true);
+	}
+}
