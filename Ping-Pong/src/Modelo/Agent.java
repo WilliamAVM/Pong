fdbf415dd984910618d84548098490c63b1861f3
alexxtpe <alexxtpe@gmail.com>
2017-06-04 21:18:29 -0400

    agente
    cambio para el agente

--- Ping-Pong/src/Modelo/Agent.java
+++ Ping-Pong/src/Modelo/Agent.java
@@ -2,9 +2,29 @@ package Modelo;
 
 
 import net.sourceforge.jFuzzyLogic.*;
-public class Agent {
 
+public class Agent {
+	private FIS fis;
+
+	public Agent(){
+		String fileName = "player.fcl";
+		fis = FIS.load(fileName,true);
+	}
 	
+	public void input(double valorX,double valorY){
+		fis.setVariable("pelotaX", valorX);
+		fis.setVariable("pelotaY", valorY);
+	}
 	
+	public void run(){
+		fis.evaluate();
+	}
 	
+	public void output(){
+
+		fis.getVariable("movimiento").getLatestDefuzzifiedValue();
+		fis.getVariable("movimiento").chartDefuzzifier(true);
+
+        System.out.println(fis);
+	}
 }
